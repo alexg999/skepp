673c8657ae386e240c8ebf821bd05ca3060399e7
alexander gustafsson <alexander.gustafsson@elev.ga.lbs.se>
2018-10-09 10:41:55 +0200

    gjorde alla c plus en A samt började på andra

--- Assets/skepp.cs
+++ Assets/skepp.cs
@@ -10,10 +10,19 @@ public class skepp : MonoBehaviour
     public Color rightColor;
     public Color leftColor;
     public float timer;
+    public float timerSec;
+    public float timerMin;
+    public float timerHours;
+    public float speed = 10;
+    public float positionX;
+    public float positionY;
 
     // Use this for initialization
     void Start()
     {
+        positionX = Random.Range(10.41f, -10.41f);
+        transform.position = new Vector3(0, 0, 0);
+        
 
     }
 
@@ -23,7 +32,7 @@ public class skepp : MonoBehaviour
         //detta gör att skeppet svänger vänster samt gör så att jag kan ändra i unity vilken färg det ska ändras till
         if (Input.GetKey(KeyCode.A))
         {
-            transform.Rotate(0f, 0f,  ((rotationSpeed - (rotationSpeed / 3)) * Time.deltaTime));
+            transform.Rotate(0f, 0f, ((rotationSpeed - (rotationSpeed / 3)) * Time.deltaTime));
             rend.color = leftColor;
         }
         //Samma som ovan fast koden gör att den svänger till höger istället för vänster
@@ -33,18 +42,51 @@ public class skepp : MonoBehaviour
             rend.color = rightColor;
         }
         //detta gör så att skeppet går framåt konstant
-        transform.Translate(10f * Time.deltaTime, 0, 0);
+
+        transform.Translate(speed * Time.deltaTime, 0, 0, Space.Self);
         //detta gör så att skeppet åker -5 steg bakåt när man håller inne S och eftersom originalspeeden var 10 så blir det 10-5=5
         if (Input.GetKey(KeyCode.S))
         {
-            transform.Translate(-5f * Time.deltaTime, 0, 0);
+            transform.Translate(-speed / 2 * Time.deltaTime, 0, 0, Space.Self);
         }
         //detta gör så att det tiden kommer upp i unity och går upp med 1 varje sekund (även fast det är kommas vid sidan)
-       timer += Time.deltaTime;
-        
-        //Debug.Log(string.Format("{0}", timer));
-        print((int)timer);
+
+        //den här gör att timern skrivs ut 1 gång per sekund och att man har minuter,
+        //detta görs för att timern fortsätter räknas och när timerSec når 60 delar timerMin timern med 60 och får ut ett helt nummer och skriver ut det.
+
+        timer += Time.deltaTime;
+        timerSec = (int)timer % 60;
+        timerMin = (int)(timer / 60) % 60;
+        timerHours = (int)(timer / 3600) % 60;
+        print(string.Format("{0:0}-{1:00}-{2:00}", timerHours, timerMin, timerSec));
+
+        //detta gör så att färgen randomisas varje gång man trycker på mellanslag
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+
+            rend.color = new Color(Random.Range(0f, 1f), Random.Range(0f, 1f), Random.Range(0f, 1f));
+        }
+        if (transform.position.x < -10.4)
+        {
+            transform.position = new Vector3(10.4f, transform.position.y, 0);
+
+        }
+        //detta gör så att om skeppet åker utanför skärmen så åker den tillbaks in fast på andra sidan
+        if (transform.position.x > 10.4)
+        {
+            transform.position = new Vector3(-10.4f, transform.position.y, 0);
+        }
+        if (transform.position.y < -5.5)
+        {
+            transform.position = new Vector3(transform.position.x, 5.5f, 0);
+
+        }
+        if (transform.position.y > 5.5)
+        {
+            transform.position = new Vector3(transform.position.x, -5.5f, 0);
+
+        }
+
     }
-    
 }
 
